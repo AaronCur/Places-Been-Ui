e36b319fae856450e4163ac4757228279695b312
AaronCur <aaron.m.curry84@gmail.com>
2026-05-22 23:15:29 +0100

    Adding vercel analytics package and changes to make app a progressive webapp with caching

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SwUpdate} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,18 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('PlacesBeenUi');
+
+  constructor(private swUpdate: SwUpdate) {}
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.subscribe(event => {
+        if (event.type === 'VERSION_READY') {
+          if (confirm('A fresh update is available! Would you like to reload the app?')) {
+            window.location.reload();
+          }
+        }
+      });
+    }
+  }
 }
